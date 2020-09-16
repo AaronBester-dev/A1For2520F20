9f9ffc8a722ff18902ebceb05b81282a5742686f
socs <socs@socsvm.local>
2020-09-16 15:35:13 -0400

    Fixed bug in getbits

--- bandb.c
+++ bandb.c
@@ -6,35 +6,31 @@
 
 
 void getbytes(unsigned char dest[], int bytes, void * src, int reverse){
-
     int i = 0;
     int k = 0;
     unsigned char * input = src;
+    
     if(reverse != 0){
        for(i = (bytes-1); i >= 0; i--){
            dest[k] = input[i];
+           
            k++;
        }
        dest[k] = '\0';
     }
     else{
-        for(i = 0; i < bytes; i++){
+        for(i = 0; i < bytes; i++){   
+            printf(" big gay%c", input[0]);
             dest[i] = input[i];
         }
         dest[i] = '\0';
     }
-
-
 }
 
-
-
-
 void getbits(char dest[], int bytes, void * src, int start, int end){
-    char * byteArray = src;
-    int i = bytes -(start/8);
+    char * byteArray = (char *)src;
+    int i = bytes - (start/8);
     int k = (start % 8);
-    printf("%d i=%d k=%d\n",sizeof(unsigned short),i,k);
     int c = 0;
     int bingus = 0;
     /*
@@ -44,10 +40,9 @@ void getbits(char dest[], int bytes, void * src, int start, int end){
     int bitEnd = end%8;
     */
     int bitsNeeded = start - (end+1);
+    while(c < bitsNeeded){
 
-    while(strlen(dest) < bitsNeeded){
-
-         bingus = (byteArray[i] >> k) & 1;
+         bingus = (int)(byteArray[i] >> k) & 1;
          
          if(bingus == 1){
              dest[c] = '1';
@@ -65,12 +60,6 @@ void getbits(char dest[], int bytes, void * src, int start, int end){
     }
     dest[c] = '\0';
 }
-
-
-
-
-
-
 
 unsigned long long bits2ull(char * bits){
 
@@ -90,10 +79,6 @@ unsigned long long bits2ull(char * bits){
     return(ull);
 
 }
-
-
-
-
 
 long long bits2ll(char * bits){
 
@@ -119,11 +104,6 @@ long long bits2ll(char * bits){
 
 }
 
-
-
-
-
-
 void spff(char * sign, char * exponent, char * significand, float * src){
     char * byteArray = (char *)src;
     int i = 3;
@@ -131,14 +111,6 @@ void spff(char * sign, char * exponent, char * significand, float * src){
   
     int c = 0;
     int bingus = 0;
-    /*
-    int byteStart = bytes -(start/8) -1;
-    int bitStart = (start%8);
-    int byteEnd = bytes-(end/8) -1;
-    int bitEnd = end%8;0.15625
-    */
-    
-
     
 
          bingus = (byteArray[i] >> k) & 1;
@@ -201,26 +173,16 @@ void dpff(char * sign, char * exponent, char * significant, double * src){
   
     int c = 0;
     int bingus = 0;
-    /*
-    int byteStart = bytes -(start/8) -1;
-    int bitStart = (start%8);
-    int byteEnd = bytes-(end/8) -1;
-    int bitEnd = end%8;0.15625
-    */
-    
-
-    
-
-         bingus = (byteArray[i] >> k) & 1;
+    bingus = (byteArray[i] >> k) & 1;
          
-         if(bingus == 1){
-             sign[0] = '1';
-         }
-         else{
-             sign[0] = '0';
-         }
-         k--;
-         sign[1] = '\0';
+    if(bingus == 1){
+        sign[0] = '1';
+    }
+    else{
+        sign[0] = '0';
+    }
+    k--;
+    sign[1] = '\0';
 
          while(c < 11){
              bingus = (byteArray[i] >> k ) & 1;
