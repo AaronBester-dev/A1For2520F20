8545ff5313d37edbc9978b61a28a079708aa8c70
socs <socs@socsvm.local>
2020-09-16 13:59:05 -0400

    Finished dpff and spff

--- bandb.c
+++ bandb.c
@@ -32,7 +32,7 @@ void getbytes(unsigned char dest[], int bytes, void * src, int reverse){
 
 void getbits(char dest[], int bytes, void * src, int start, int end){
     char * byteArray = src;
-    int i = bytes -(start/8)   ;
+    int i = bytes -(start/8);
     int k = (start % 8);
     printf("%d i=%d k=%d\n",sizeof(unsigned short),i,k);
     int c = 0;
@@ -64,20 +64,6 @@ void getbits(char dest[], int bytes, void * src, int start, int end){
          c++;
     }
     dest[c] = '\0';
-/*
-    for(i = 0; i<=byteEnd; i++){
-        if(i = 0){
-            for(k = bitStart; k >=0; k--){
-
-            }
-        }
-        else{
-            for(k = 8; k>=)
-        }
-
-    }
-*/
-
 }
 
 
@@ -139,7 +125,69 @@ long long bits2ll(char * bits){
 
 
 void spff(char * sign, char * exponent, char * significand, float * src){
+    char * byteArray = (char *)src;
+    int i = 3;
+    int k = 7;
+  
+    int c = 0;
+    int bingus = 0;
+    /*
+    int byteStart = bytes -(start/8) -1;
+    int bitStart = (start%8);
+    int byteEnd = bytes-(end/8) -1;
+    int bitEnd = end%8;0.15625
+    */
+    
 
+    
+
+         bingus = (byteArray[i] >> k) & 1;
+         
+         if(bingus == 1){
+             sign[0] = '1';
+         }
+         else{
+             sign[0] = '0';
+         }
+         k--;
+         sign[1] = '\0';
+
+         while(c < 8){
+             bingus = (byteArray[i] >> k ) & 1;
+            
+            if(bingus == 1){
+                exponent[c] = '1';
+            }
+            else{
+                exponent[c] = '0';
+            }
+            k--;
+            if(k < 0){
+                k = 7;
+                i--;
+            }
+            c++;
+         }
+         exponent[c] = '\0';
+        c = 0;
+         while(c < 23){
+              bingus = (byteArray[i] >> k ) & 1;
+            
+            if(bingus == 1){
+                significand[c] = '1';
+            }
+            else{
+                significand[c] = '0';
+            }
+            k--;
+            if(k < 0){
+                k = 7;
+                i--;
+            }
+            c++;
+         }
+   
+    significand[c] = '\0';
 }
 
 
@@ -147,6 +195,68 @@ void spff(char * sign, char * exponent, char * significand, float * src){
 
 
 void dpff(char * sign, char * exponent, char * significant, double * src){
+    char * byteArray = (char *)src;
+    int i = 7;
+    int k = 7;
+  
+    int c = 0;
+    int bingus = 0;
+    /*
+    int byteStart = bytes -(start/8) -1;
+    int bitStart = (start%8);
+    int byteEnd = bytes-(end/8) -1;
+    int bitEnd = end%8;0.15625
+    */
+    
 
+    
+
+         bingus = (byteArray[i] >> k) & 1;
+         
+         if(bingus == 1){
+             sign[0] = '1';
+         }
+         else{
+             sign[0] = '0';
+         }
+         k--;
+         sign[1] = '\0';
+
+         while(c < 11){
+             bingus = (byteArray[i] >> k ) & 1;
+            
+            if(bingus == 1){
+                exponent[c] = '1';
+            }
+            else{
+                exponent[c] = '0';
+            }
+            k--;
+            if(k < 0){
+                k = 7;
+                i--;
+            }
+            c++;
+         }
+         exponent[c] = '\0';
+        c = 0;
+         while(c < 52){
+              bingus = (byteArray[i] >> k ) & 1;
+            
+            if(bingus == 1){
+                significant[c] = '1';
+            }
+            else{
+                significant[c] = '0';
+            }
+            k--;
+            if(k < 0){
+                k = 7;
+                i--;
+            }
+            c++;
+         }
+   
+    significant[c] = '\0';
 }
 
