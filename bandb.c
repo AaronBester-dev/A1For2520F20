afafcbfe8a9b5d415528ad858162c7ef29df6c87
socs <socs@socsvm.local>
2020-09-20 14:45:19 -0400

    Added comments to bandb.c

--- bandb.c
+++ bandb.c
@@ -3,11 +3,12 @@
 #include <string.h>
 #include "bandb.h"
 
+/*Gets the bytes from any source and takes each byte from source and puts it in a char string */
 void getbytes(unsigned char dest[], int bytes, void * src, int reverse){
     int i = 0;
     int k = 0;
     unsigned char * input = src;
-    
+    /*Gets the string in reverse by taking the rightmost bit needed first instead of the left.*/
     if(reverse != 0){
        for(i = (bytes-1); i >= 0; i--){
            dest[k] = input[i];
@@ -15,6 +16,7 @@ void getbytes(unsigned char dest[], int bytes, void * src, int reverse){
        }
        dest[k] = '\0';
     }
+    /*Otherwise just gets the bits needed in order*/
     else{
         for(i = 0; i < bytes; i++){   
             dest[i] = input[i];
@@ -23,6 +25,7 @@ void getbytes(unsigned char dest[], int bytes, void * src, int reverse){
     }
 }
 
+/*Gets each individual bits from a source and prints each bit as a char in a string */
 void getbits(char dest[], int bytes, void * src, int start, int end){
     unsigned char * byteArray = (unsigned char *)src;
     int i = bytes - (start/8) - 1;
@@ -31,6 +34,8 @@ void getbits(char dest[], int bytes, void * src, int start, int end){
     int bitsNeeded = start - end;
     
     while(c < bitsNeeded){
+        /*Shifts the bit needed into the rightmost position so we can check it with the 1 bit and see whether its on or off */
+        /*The + '0' addition simply adds the ascii code of 0 to the original value in order to change it from a int to a char */
          dest[c] = ((byteArray[i] >> k) & 1) + '0';
 
          k--;
@@ -43,6 +48,7 @@ void getbits(char dest[], int bytes, void * src, int start, int end){
     dest[c] = '\0';
 }
 
+/*Takes a string of bits and converts it into its decimal value. */
 unsigned long long bits2ull(char * bits){
     int i = strlen(bits)-1;
     int k = 0;
@@ -50,6 +56,7 @@ unsigned long long bits2ull(char * bits){
     unsigned long long ull = 0;
 
     while(bits[k] != '\0'){
+        /*Takes every bit in bit string and multiplies it by 2 ^ of the bit position */;
         ull += (bits[k]-'0') * ((unsigned long long)1 << i); 
         i--;
         k++;
@@ -58,17 +65,18 @@ unsigned long long bits2ull(char * bits){
     return(ull);
 
 }
-
+/*Takes a string of two's complement bits and converts it into its decimal value. */
 long long bits2ll(char * bits){
     int i = strlen(bits)-1;
     int k = 0;
     long long ll = 0;
-
+    /*Simply takes the first bit and makes it negative.*/
     ll += (-1) * (bits[k]-'0') * ((long long)1 << i);
     i--;
     k++;
 
     while(bits[k] != '\0'){
+        /*Takes every bit in bit string and multiplies it by 2 ^ of the bit position */;
         ll += (bits[k]-'0') * ((long long)1 << i);
         i--;
         k++;
@@ -80,6 +88,7 @@ long long bits2ll(char * bits){
 
 void spff(char * sign, char * exponent, char * significand, float * src){
     unsigned char dest[5];
+    /*getbytes is only used to reverse the bytes in the src string so that it shows up forwards when using getbits*/
     getbytes(dest,4,src,1);
     getbits(sign,4,dest,31,30);
     getbits(exponent,4,dest,30,22);
@@ -87,6 +96,7 @@ void spff(char * sign, char * exponent, char * significand, float * src){
 }
 
 void dpff(char * sign, char * exponent, char * significant, double * src){
+    /*getbytes is only used to reverse the bytes in the src string so that it shows up forwards when using getbits*/
     unsigned char dest[9];
     getbytes(dest,8,src,1);
     getbits(sign,8,dest,63,62);
